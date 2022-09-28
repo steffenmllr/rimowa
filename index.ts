4c8de526e9a2b573a69873cd15d0ed9f2e4e7a2e
steffenmllr <steffen@mllrsohn.com>
2022-09-28 22:43:46 +0200

    check result

--- index.ts
+++ index.ts
@@ -3,25 +3,63 @@ import {
     DOMParser
 } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
-const getData = async () => {
-  const text = await fetch(
-    "https://www.rimowa.com/de/de/luggage/colour/blue/cabin/83253814.html"
-  ).then((res) => res.text());
+const getData = async (url: string) => {
+
+
+  const text = await fetch(url).then((res) => res.text());
   const document = new DOMParser().parseFromString(text,"text/html",);
   const av = document?.getElementsByClassName('js-product-availability')
   if (!av || av.length === 0) {
     return "nicht verfügbar";
   }
-  return av[0].innerText
+  const result = av[0].innerText.trim();
+  console.log(`Checking: ${url} - ${result}`);
+
+  const apiKey = "7d3e57fd32af2e46ad2f467fa9e30865";
+  const secretKey = "428b4aed2dbae9f09be9703a966c08d1";
+
+  if (result !== "Nicht verfügbar") {
+    const paylod = {
+        Messages: [
+          {
+            From: {
+              Email: "steffen@mllrsohn.com",
+              Name: "Steffen Müller",
+            },
+            To: [
+              {
+                Email: "hello@steffen.io",
+                Name: "Steffen",
+              },
+              {
+                Email: 'lottalampenschirm@me.com',
+                Name: "Lotta",
+              }
+            ],
+            Subject: "Rimowa verfügbar",
+            TextPart: `und zwar hier: ${url}`,
+            HTMLPart: `und zwar hier: ${url}`,
+          },
+        ],
+      };
+      await fetch("https://api.mailjet.com/v3.1/send", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Basic " + btoa(apiKey + ":" + secretKey),
+          Accept: "application/json",
+        },
+        body: JSON.stringify(paylod),
+      }).then((r) => r.json());
+  }
+
 };
 
 async function handler(_req: Request) {
-  const text = await fetch(
-    "https://www.rimowa.com/de/de/luggage/colour/white/cabin/83253664.html"
-  ).then((res) => res.text());
-const stock = await getData();
+  await getData("https://www.rimowa.com/de/de/luggage/colour/blue/cabin/83253814.html");
+  await getData("https://www.rimowa.com/de/de/luggage/colour/blue/cabin/83253614.html");
   const data = {
-    stock
+    stock: 'ok'
   };
   const body = JSON.stringify(data, null, 2);
   return new Response(body, {
@@ -30,4 +68,4 @@ const stock = await getData();
 }
 
 serve(handler);
-// await getData();
+
